059dd004c053b9495690e3aa33c93680a42a2998
Julien <julien.paris.casset@gmail.com>
2015-11-04 13:20:55 +0100

    Initial commit

---
+++ src/dessin/ObjetGraphique.java
@@ -0,0 +1,32 @@
+package dessin;
+
+import java.awt.Color;
+
+/**
+ * Created by Julien on 04/11/2015.
+ */
+public abstract class ObjetGraphique {
+    private Color couleur;
+
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    public ObjetGraphique(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    public ObjetGraphique() {
+        this.couleur = Color.black;
+    }
+
+    public abstract void dessineToi(java.awt.Graphics graphics);
+
+    public abstract boolean contient(int entier1, int entier2);
+
+
+}
