e521baf1a7b43462e88fde1ecaa345c88b7c97e7
Julien <julien.paris.casset@gmail.com>
2015-11-05 15:05:43 +0100

    Edit Rectangle

--- src/dessin/Rectangle.java
+++ src/dessin/Rectangle.java
@@ -1,7 +1,86 @@
 package dessin;
 
+import java.awt.*;
+
 /**
  * Created by Julien on 04/11/2015.
  */
-public class Rectangle {
+
+public class Rectangle extends ObjetGraphique {
+
+    private java.awt.Rectangle rectangle;
+    private int hauteurRect;
+    private int largeurRect;
+    private Color couleur;
+    private Point pointRect;
+
+    @Override
+    public void dessineToi(Graphics graphics) {
+
+    }
+
+    @Override
+    public boolean contient(int entier1, int entier2) {
+        return false;
+    }
+
+    public Rectangle(int x, int y, int largeur, int hauteur) {
+        hauteurRect = hauteur;
+        largeurRect = largeur;
+    }
+
+    public Rectangle(Point p, int largeur, int hauteur) {
+        pointRect = p;
+        largeurRect = largeur;
+        hauteurRect = hauteur;
+    }
+
+    public Rectangle(Point p, int largeur, int hauteur, Color c) {
+        largeurRect = largeur;
+        hauteurRect = hauteur;
+        couleur = c;
+        pointRect = p;
+    }
+
+    public java.awt.Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setRectangle(java.awt.Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public int getHauteurRect() {
+        return hauteurRect;
+    }
+
+    public void setHauteurRect(int hauteurRect) {
+        this.hauteurRect = hauteurRect;
+    }
+
+    public int getLargeurRect() {
+        return largeurRect;
+    }
+
+    public void setLargeurRect(int largeurRect) {
+        this.largeurRect = largeurRect;
+    }
+
+    @Override
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    @Override
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    public Point getPointRect() {
+        return pointRect;
+    }
+
+    public void setPointRect(Point pointRect) {
+        this.pointRect = pointRect;
+    }
 }
