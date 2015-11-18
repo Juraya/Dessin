dd5a872971aefbe75a4be9fc3f1b30951c06b2c9
Julien <julien.paris.casset@gmail.com>
2015-11-18 11:39:40 +0100

    Javadoc cercle

--- src/dessin/Cercle.java
+++ src/dessin/Cercle.java
@@ -7,25 +7,55 @@ import java.awt.*;
  */
 public class Cercle extends ObjetGraphique {
 
+    /**
+     * Rayon du cercle
+     */
     private int rayonCer;
+    /**
+     * Point de départ du cercle
+     */
     private Point pointCer;
+    /**
+     * Couleur du cercle
+     */
     private Color couleurCer;
 
+    /**
+     * Méthode qui permet de dessiner le cercle, en passant en paramètre des coordonnées de début et un rayon.
+     * @param graphics
+     */
     @Override
     public void dessineToi(Graphics graphics) {
         graphics.drawOval(pointCer.x, pointCer.y, rayonCer, rayonCer);
     }
 
+    /**
+     * Méthode qui permet de savoir si le point est contenu dans la zone.
+     * @param entier1
+     * @param entier2
+     * @return
+     */
     @Override
     public boolean contient(int entier1, int entier2) {
         return false;
     }
 
+    /**
+     * Constructeur sans la couleur.
+     * @param centre
+     * @param rayon
+     */
     public Cercle(Point centre,int rayon) {
         rayonCer = rayon;
         pointCer = centre;
     }
 
+    /**
+     * Constructeur du cercle en permettant de mettre une couleur.
+     * @param centre
+     * @param rayon
+     * @param couleur
+     */
     public Cercle(Point centre, int rayon, Color couleur) {
         pointCer = centre;
         rayonCer = rayon;
